575593f2b25be77cef2755604d3ec019ce7c225a
maciejbaba <maciek2baba2@gmail.com>
2024-04-12 13:07:51 +0200

    shimi

---
+++ cpp/853-car-fleet.cpp
@@ -0,0 +1,13 @@
+ // 0 1 2 3 4 5 6 7 8 9 10 11 12
+ // 1     3   1     4    2     
+ 
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+
+
+
+    }
+};
---
+++ javascript/853-car-fleet.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ */
+let carFleet = function(target, position, speed) {
+  let currLead;
+  let currLeadUpdated = 0;
+  let n = position.length;
+  
+  for (let i = n; i > 0; i++) {
+    
+}
+    
+};
