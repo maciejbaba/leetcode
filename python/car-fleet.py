e88019eaf8b9e70b39b8c09d0a8c2d1cad430bcc
maciejbaba <maciek2baba2@gmail.com>
2024-05-04 17:38:47 +0200

    car-fleet

--- python/car-fleet.py
+++ python/car-fleet.py
@@ -1,3 +1,20 @@
+from typing import List
+
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        
+        pair = [(p, s) for p, s in zip(position, speed)]
+        print(pair)
+        pair.sort(reverse=True)
+        print(pair)
+        stack = []
+        for p, s in pair:
+            print("position", p,"speed", s)
+            stack.append((target - p) / s)
+            print("target", target)
+            print("position", p)
+            print("(target - p) / s", (target - p) / s)
+            print("stack", stack)
+            if len(stack) >= 2 and stack[-1] <= stack [-2]:
+                stack.pop()
+        return len(stack)
+
